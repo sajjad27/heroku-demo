c1149486721d5089cd47743742aeb064cc200172
sajjad27 <sajad-2020@hotmail.com>
2023-11-28 19:23:08 +0300

    build the app first

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve static files from the 'dist' directory
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// Catch all other routes and return the 'index.html' file
+// Serve the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Start the app on the process.env.PORT (Heroku sets this automatically)
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
